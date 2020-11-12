3ef4cc1dcf7227ada97ab72e3505a5fd68da2f99
Letícia Rose <leticia.rosedesantana@gmail.com>
2020-11-12 15:16:04 -0300

    Teste de software unitário
    Atividade 15 da disciplina de QNT- 2020, tipos de teste

---
+++ media.js
@@ -0,0 +1,7 @@
+const media = (a,b) => {
+    return Number ((a+b)/2)
+}
+// console.log (media(4,4)) 
+
+module.exports = media 
+
---
+++ media.test.js
@@ -0,0 +1,5 @@
+const media = require ("./media") 
+
+test ("verificarMedia",()=>{
+    expect (media (4,4)).toBe(4)
+})
